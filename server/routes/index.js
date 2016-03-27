f6b0680aaae38910470d184e6f00f11401c3982d
Carl Peaslee <cpeaslee@gmail.com>
2016-03-26 20:25:57 -0500

    removed some db stuff to help heroku

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,11 +4,6 @@ var path = require("path");
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
-mongoose.connect('mongodb://localhost/carl_rsj0_1_0');
-mongoose.model("book", new Schema({"title" : String, "author" : String, "pubdate" : String, "imageurl" : String,}));
-var Book = mongoose.model("book");
-
-
 
 router.get("/*", function(req,res){
     var file = req.params[0] || "/views/index.html";
